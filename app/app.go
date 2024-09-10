f9172e16a757cce39fd5160825bc36cf7cc78295
Полина Кузнецова <polinakuznecova@MacBook-Pro-Polina.local>
2024-09-10 10:14:15 +0500

    added app logic implementation

---
+++ app/app.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"task-tracker/models"
+	"task-tracker/storage"
+)
+
+type TaskService interface {
+	AddTask(task string)
+	UpdateTask(id int, task string)
+	DeleteTask(id int)
+	ListAllTasks() []models.Task
+	ListDoneTasks() []models.Task
+	ListProgressTasks() []models.Task
+	ListToDoTasks() []models.Task
+	MarkInProgress(id int)
+	MarkDone(id int)
+	MarkToDo(id int)
+}
+
+type App struct {
+	Storage storage.Storage
+}
+
+func NewApp(storage storage.Storage) *App {
+	return &App{Storage: storage}
+}
---
+++ app/service.go
@@ -0,0 +1,116 @@
+package app
+
+import (
+	"task-tracker/models"
+	"time"
+)
+
+var counter int
+
+func (a *App) AddTask(task string) {
+	counter++
+	data := models.Task{
+		Id:          counter,
+		Description: task,
+		Status:      "todo",
+		CreatedAt:   time.Now(),
+	}
+	a.Storage.SaveInfo(data)
+
+}
+
+func (a *App) UpdateTask(id int, task string) {
+	fileTasks := a.Storage.LoadInfo()
+	for i, _ := range fileTasks {
+		if fileTasks[i].Id == id {
+			fileTasks[i].Description = task
+			fileTasks[i].UpdatedAt = time.Now()
+			break
+		}
+	}
+	a.Storage.UpdateInfo(fileTasks)
+}
+
+func (a *App) DeleteTask(id int) {
+	fileTasks := a.Storage.LoadInfo()
+	for i, _ := range fileTasks {
+		if fileTasks[i].Id == id {
+			fileTasks = append(fileTasks[:i], fileTasks[i+1:]...)
+		}
+	}
+	a.Storage.UpdateInfo(fileTasks)
+}
+
+func (a *App) ListAllTasks() []models.Task {
+	fileTasks := a.Storage.LoadInfo()
+	return fileTasks
+}
+
+func (a *App) ListDoneTasks() []models.Task {
+	fileTasks := a.Storage.LoadInfo()
+	var doneTasks []models.Task
+	for i, _ := range fileTasks {
+		if fileTasks[i].Status == "done" {
+			doneTasks = append(doneTasks, fileTasks[i])
+		}
+	}
+	return doneTasks
+}
+
+func (a *App) ListToDoTasks() []models.Task {
+	fileTasks := a.Storage.LoadInfo()
+	var toDoTasks []models.Task
+	for i, _ := range fileTasks {
+		if fileTasks[i].Status == "todo" {
+			toDoTasks = append(toDoTasks, fileTasks[i])
+		}
+	}
+	return toDoTasks
+}
+
+func (a *App) ListProgressTasks() []models.Task {
+	fileTasks := a.Storage.LoadInfo()
+	var ProgressTasks []models.Task
+	for i, _ := range fileTasks {
+		if fileTasks[i].Status == "in-progress" {
+			ProgressTasks = append(ProgressTasks, fileTasks[i])
+		}
+	}
+	return ProgressTasks
+}
+
+func (a *App) MarkInProgress(id int) {
+	fileTasks := a.Storage.LoadInfo()
+	for i, _ := range fileTasks {
+		if fileTasks[i].Id == id {
+			fileTasks[i].Status = "in-progress"
+			fileTasks[i].UpdatedAt = time.Now()
+			break
+		}
+	}
+	a.Storage.UpdateInfo(fileTasks)
+}
+
+func (a *App) MarkDone(id int) {
+	fileTasks := a.Storage.LoadInfo()
+	for i, _ := range fileTasks {
+		if fileTasks[i].Id == id {
+			fileTasks[i].Status = "done"
+			fileTasks[i].UpdatedAt = time.Now()
+			break
+		}
+	}
+	a.Storage.UpdateInfo(fileTasks)
+}
+
+func (a *App) MarkToDo(id int) {
+	fileTasks := a.Storage.LoadInfo()
+	for i, _ := range fileTasks {
+		if fileTasks[i].Id == id {
+			fileTasks[i].Status = "todo"
+			fileTasks[i].UpdatedAt = time.Now()
+			break
+		}
+	}
+	a.Storage.UpdateInfo(fileTasks)
+}
